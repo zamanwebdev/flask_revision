07510607d34b822d460387670cefff5480bdc763
Syed Zaman <zamanwebdev@gmail.com>
2026-02-20 15:01:40 +0600

    Day 2 - Routing Deep Dive + Dynamic URL = Done.

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask
 
-# Flask app তৈরি
 app = Flask(__name__)
 
 # Home Route
 @app.route('/')
 def home():
-    return "<h1>Welcome Syed Zaman 🔥</h1>"
+    return "<h1>Welcome to Flask Revision 🔥</h1>"
+
+# About Route
 @app.route('/about')
 def about():
     return "<h2>This is About Page</h2>"
 
+# Dynamic String Route
+@app.route('/user/<name>')
+def user(name):
+    return f"<h3>Hello {name}, Welcome to Flask!</h3>"
 
-# Run Server
+# Dynamic Integer Route
+@app.route('/number/<int:num>')
+def number(num):
+    return f"<h3>You entered number: {num}</h3>"
+
 if __name__ == '__main__':
     app.run(debug=True)
