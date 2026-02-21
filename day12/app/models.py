d264d516a85a796c6f5470978e364ca19505dc0e
Syed Zaman <zamanwebdev@gmail.com>
2026-02-21 19:12:20 +0600

    Day 12 - Login System + Session (Full Working Code) = Done.

---
+++ day12/app/models.py
@@ -0,0 +1,13 @@
+from . import db
+from datetime import datetime
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
